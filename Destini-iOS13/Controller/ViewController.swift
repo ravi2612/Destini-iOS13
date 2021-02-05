72a12f86cc67d70962a0093694ab24beb17f36a0
ravi <ravinavarro26@gmail.com>
2021-02-04 22:00:19 -0300

    Questions Model

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,13 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storybrain = StoryBrain()
+    
     override func viewDidLoad() {
+        _ = Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right", choice1Destination:  1, choice2Destination: 1)
+        
+        
+        
         super.viewDidLoad()
 
+    }
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
     }
 
 
